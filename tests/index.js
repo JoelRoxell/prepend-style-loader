8d0ba4949ec3d7c4c98ba6f2d671c151d79c1301
Joel Roxell <joel.roxell@annevo.com>
2017-07-10 11:46:29 +0200

    implemented options support: string | Array<string>

--- tests/index.js
+++ tests/index.js
@@ -15,4 +15,28 @@ describe('Prepend style loader', function() {
     expect(fileContent).to.contain(`@import './tests/src/resources/global/mixins';`);
     expect(fileContent).to.contain(`@import './tests/src/resources/global/variables';`);
   });
+
+  it('should append a single file', () => {
+    const fileContent = prependStyleLoader.call(
+      {
+          cacheaible: function() {},
+          query: '?prepend=./tests/src/resources/global/variables'
+      }
+      , '.wrapper { color: red; }');
+
+    expect(fileContent).to.contain(`.wrapper { color: red; }`);
+    expect(fileContent).to.contain(`@import './tests/src/resources/global/variables';`);
+  });
+
+  it('should throw a descriptive error message', () => {
+    try {
+      const fileContent = prependStyleLoader.call(
+        {
+            cacheaible: function() {}
+        }
+        , '.wrapper { color: red; }');
+    } catch (e) {
+      expect(e.message).to.exist;
+    }
+  });
 });
